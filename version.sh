10da6f303969b44ea45a28546b5fcb643709fca0
Robert Nelson <robertcnelson@gmail.com>
2015-07-30 14:53:54 -0500

    push out hte bisector
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

---
+++ version.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+ARCH=$(uname -m)
+
+config="omap2plus_defconfig"
+
+#toolchain="gcc_linaro_eabi_4_8"
+#toolchain="gcc_linaro_eabi_4_9"
+#toolchain="gcc_linaro_gnueabi_4_6"
+#toolchain="gcc_linaro_gnueabihf_4_7"
+#toolchain="gcc_linaro_gnueabihf_4_8"
+toolchain="gcc_linaro_gnueabihf_4_9"
+
+#Kernel/Build
+KERNEL_REL=4.0
+KERNEL_TAG=${KERNEL_REL}
+BUILD=x0
+
+#v3.X-rcX + upto SHA
+#prev_KERNEL_SHA=""
+#KERNEL_SHA=""
+
+#git branch
+#BRANCH="v4.X.x"
+
+DISTRO=cross
+DEBARCH=armhf
+#
