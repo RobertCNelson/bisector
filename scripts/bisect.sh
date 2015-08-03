e1a376e82fc15617f29540d7730dae258bd28cc6
Robert Nelson <robertcnelson@gmail.com>
2015-08-03 09:15:03 -0500

    add two more bad
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/bisect.sh
+++ scripts/bisect.sh
@@ -9,6 +9,10 @@ git bisect start
 git bisect good v4.0
 git bisect bad v4.1-rc1
 
+#2/6 reboots...
+git bisect bad 6c373ca89399c5a3f7ef210ad8f63dc3437da345
+#2 reboots...
+git bisect bad e95e7f627062be5e6ce971ce873e6234c91ffc50
 
 git describe
 cd ${DIR}/
