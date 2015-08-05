b036d10abfe6118fca96b00b9e321d691df71d1e
Robert Nelson <robertcnelson@gmail.com>
2015-08-05 15:11:14 -0500

    setup next two
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/bisect.sh
+++ scripts/bisect.sh
@@ -25,7 +25,7 @@ git bisect good 1a370f4cd95e056d55ef5bf1a183880e70195e59 # v4.0-682-g1a370f4
 #boards: 1 & 2...
 git bisect good 3be1b98e073bdd4c1bb3144201a927c4a21330ba # v4.0-1013-g3be1b98
 
-#boards: 3 & 4...
+#board 3 rebooted...
 git bisect bad c6ab3aec4bc4beda2d6eb8ea43c6f5be3b215d3f # v4.0-rc3-96-gab330cf
 
 #boards: 7 & 8...
@@ -35,6 +35,16 @@ git bisect bad ab330cf3888d8e0779fa05a243d53ba9f53a7ba9 # v4.0-rc3-48-g44b3165
 #git bisect good 3be1b98e073bdd4c1bb3144201a927c4a21330ba # v4.0-1013-g3be1b98
 #git bisect bad c6ab3aec4bc4beda2d6eb8ea43c6f5be3b215d3f # v4.0-rc5-193-gc6ab3ae
 #git bisect bad ab330cf3888d8e0779fa05a243d53ba9f53a7ba9 # v4.0-rc3-96-gab330cf
+
+#testing: board 3
+#git bisect good 44b316525986252bb95d356419fc9e75f0532112
+
+#testing: board 4
+#git bisect bad 44b316525986252bb95d356419fc9e75f0532112
+
+#git bisect good
+#git bisect log
+#exit 2
 
 git describe
 cd ${DIR}/
