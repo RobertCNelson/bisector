330c0bb59d89056708ba39421685481338fafbd6
Robert Nelson <robertcnelson@gmail.com>
2015-08-05 09:16:44 -0500

    add this one
    Signed-off-by: Robert Nelson <robertcnelson@gmail.com>

--- scripts/bisect.sh
+++ scripts/bisect.sh
@@ -28,8 +28,8 @@ git bisect good 3be1b98e073bdd4c1bb3144201a927c4a21330ba # v4.0-1013-g3be1b98
 #boards: 3 & 4...
 git bisect bad c6ab3aec4bc4beda2d6eb8ea43c6f5be3b215d3f # v4.0-rc3-96-gab330cf
 
-#boards: 5...
-#git bisect bad ab330cf3888d8e0779fa05a243d53ba9f53a7ba9 # v4.0-rc3-96-gab330cf
+#boards: 7 & 8...
+git bisect bad ab330cf3888d8e0779fa05a243d53ba9f53a7ba9 # v4.0-rc3-96-gab330cf
 
 #nuno's results:
 #git bisect good 3be1b98e073bdd4c1bb3144201a927c4a21330ba # v4.0-1013-g3be1b98
